611ce472941d92619ec215f7c34e595b91cb6ecc
ssrlive <30760636+ssrlive@users.noreply.github.com>
2024-07-25 16:01:36 +0800

    CRUD basic

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,55 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+
+    // $users = DB::table('users')->get();
+    // dd($users);
+
+
+    // query data
+
+    // $users = DB::select('select * from users where email = ?', ['a@b.com']);
+    // dd($users);
+
+    // $user = DB::table('users')->where('email', 'a@b.com')->first();
+    // dd($user);
+
+
+    // insert data
+
+    // $user = DB::insert('insert into users (name, email, password) values (?, ?, ?)', ['John Doe', 'johndoe@gmail.com', 'password']);
+    // dd($user);
+
+    // $user = DB::table('users')->insert([
+    //     'name' => 'Jane Doe',
+    //     'email' => 'johndoe@gmail.com',
+    //     'password' => 'password',
+    // ]);
+    // dd($user);
+
+
+    // update data
+
+    // $user = DB::update('update users set name = ? where id = ?', ['Jane Doe', 3]);
+    // dd($user);
+
+    // $user = DB::table('users')->where('id', 4)->update([
+    //     'name' => 'XxxxVvvv',
+    // ]);
+    // dd($user);
+
+
+    // delete data
+
+    // $user = DB::delete('delete from users where id = ?', [3]);
+    // dd($user);
+
+    // $user = DB::table('users')->where('id', 4)->delete();
+    // dd($user);
 });
 
 Route::get('/dashboard', function () {
